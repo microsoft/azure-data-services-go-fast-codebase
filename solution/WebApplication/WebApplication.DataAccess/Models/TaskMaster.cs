973bf5e8d2f5f8d5ff7bc9c604d59cc8647f1fa4
Dylan Smith <dylan.smith@arkahna.io>
2021-12-29 11:39:14 +0800

    Validation Check Task Type
    Created validation for Task Type

--- solution/WebApplication/WebApplication.DataAccess/Models/TaskMaster.cs
+++ solution/WebApplication/WebApplication.DataAccess/Models/TaskMaster.cs
@@ -5,18 +5,27 @@ namespace WebApplication.Models
     public partial class TaskMaster
     {
         public long TaskMasterId { get; set; }
+        [Display(Name = "Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please input valid Name")]
         public string TaskMasterName { get; set; }
+        [Display(Name = "Task Type")]
         public int TaskTypeId { get; set; }
+        [Display(Name = "Task Group")]
         public long TaskGroupId { get; set; }
+        [Display(Name = "Schedule Master")]
         public long ScheduleMasterId { get; set; }
+        [Display(Name = "Source System")]
         public long SourceSystemId { get; set; }
+        [Display(Name = "Target System")]
         public long TargetSystemId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please input valid Degree Of Parallelism")]
         public int DegreeOfCopyParallelism { get; set; }
         public bool AllowMultipleActiveInstances { get; set; }
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Please input a DataFactory")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please input valid DataFactory")]
         public string TaskDatafactoryIr { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please input valid json data")]
         public string TaskMasterJson { get; set; }
+        [Display(Name = "Is Active")]
         public bool ActiveYn { get; set; }
         public string DependencyChainTag { get; set; }
         public long DataFactoryId { get; set; }
