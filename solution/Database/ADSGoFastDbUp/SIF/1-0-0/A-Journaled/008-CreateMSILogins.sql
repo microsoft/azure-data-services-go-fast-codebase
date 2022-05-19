fb01d4dbe2b1564591a44f867a9951aaeb9c9516
Leonardo Bravo <lbravo@moqdigital.com.au>
2022-05-19 12:41:58 +1000

    sif files

--- solution/Database/ADSGoFastDbUp/SIF/1-0-0/A-Journaled/008-CreateMSILogins.sql
+++ solution/Database/ADSGoFastDbUp/SIF/1-0-0/A-Journaled/008-CreateMSILogins.sql
@@ -1,15 +1,16 @@
-        DROP USER IF EXISTS [$FunctionAppName$] 
-        CREATE USER [$FunctionAppName$] FROM EXTERNAL PROVIDER;
-        ALTER ROLE db_datareader ADD MEMBER [$FunctionAppName$];
-        ALTER ROLE db_datawriter ADD MEMBER [$FunctionAppName$];
-        ALTER ROLE db_ddladmin ADD MEMBER [$FunctionAppName$];
-        GRANT EXECUTE ON SCHEMA::[dbo] TO [$FunctionAppName$];
-        GO 
+        --DROP USER IF EXISTS [$FunctionAppName$] 
+        --CREATE USER [$FunctionAppName$] FROM EXTERNAL PROVIDER;
+        --ALTER ROLE db_datareader ADD MEMBER [$FunctionAppName$];
+        --ALTER ROLE db_datawriter ADD MEMBER [$FunctionAppName$];
+        --ALTER ROLE db_ddladmin ADD MEMBER [$FunctionAppName$];
+        --GRANT EXECUTE ON SCHEMA::[dbo] TO [$FunctionAppName$];
+        --GO 
 
         DROP USER IF EXISTS [$WebAppName$] 
         CREATE USER [$WebAppName$] FROM EXTERNAL PROVIDER;
         ALTER ROLE db_datareader ADD MEMBER [$WebAppName$];
         ALTER ROLE db_datawriter ADD MEMBER [$WebAppName$];
+		ALTER ROLE db_ddladmin ADD MEMBER [$FunctionAppName$];
         GRANT EXECUTE ON SCHEMA::[dbo] TO [$WebAppName$];
         GO
 
